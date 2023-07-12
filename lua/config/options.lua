8452c8258acaf1a4b042fc43aefbcb97cdaea6f6
Jared Johnston <jazzab@gmail.com>
2023-07-12 15:59:01 +1000

    Change local leader and install gruvbox

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- set here because leader must be set before lazy.nvim
+-- vim.keymap.set("n", ",", "<Nop>", { silent = true })
+vim.g.mapleader = ","
--- lua/plugins/jj_other.lua
+++ lua/plugins/jj_other.lua
@@ -1,4 +1,14 @@
 return {
   -- escape without lagging
   { "nvim-zh/better-escape.vim" },
+
+  -- load gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
 }
