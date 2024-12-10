3b56711733a7925623a80b96bee301c662073098
Jared Johnston <j.johnston@aims.gov.au>
2024-12-10 13:00:00 +1000

    Add hints when starting

--- init.lua
+++ init.lua
@@ -2,5 +2,9 @@
 require("config.lazy")
 
 require("notify")(
-  "Some useful shortcuts: <leader>rg, <leader>ff, <leader>/, <leader>fr. Also use :Notifications to see a history of them"
+  "Some useful shortcuts: \n"..
+    "end<leader>rg, <leader>/, <leader>fr. Also use :Notifications to see a history of them"
+)
+require("notify")(
+  "use Q with macros (:h Q for more info)"
 )
